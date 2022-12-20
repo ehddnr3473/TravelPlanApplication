767a4d280d143171db2c0936783628a9d3503059
Donguk Kim <ehddnr73@naver.com>
2022-12-20 12:09:33 +0900

    PlanView 초기 구성

--- YeolmokTravel/YeolmokTravel/Todo/PlanView.swift
+++ YeolmokTravel/YeolmokTravel/Todo/PlanView.swift
@@ -6,24 +6,108 @@
 //
 
 import UIKit
+import SnapKit
 
 class PlanView: UIViewController {
-
+    // MARK: - Properties
+    private var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = TextConstants.title
+        label.font = .boldSystemFont(ofSize: AppStyles.titleFontSize)
+        
+        return label
+    }()
+    
+    private lazy var addButton: UIButton = {
+        let button = UIButton(type: .custom)
+        
+        button.setBackgroundImage(UIImage(systemName: TextConstants.plus), for: .normal)
+        button.tintColor = AppStyles.mainColor
+        button.addTarget(self, action: #selector(touchUpAddButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private var planTableView: UITableView = {
+        let tableView = UITableView()
+        
+        tableView.register(PlanTableViewCell.self, forCellReuseIdentifier: PlanTableViewCell.identifier)
+        tableView.backgroundColor = .black
+        tableView.layer.cornerRadius = LayoutConstants.cornerRadius
+        tableView.layer.borderWidth = LayoutConstants.borderWidth
+        tableView.layer.borderColor = UIColor.white.cgColor
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
+        configure()
+    }
+}
 
-        // Do any additional setup after loading the view.
+// MARK: - SetUp View
+extension PlanView {
+    private func setUpUI() {
+        view.backgroundColor = .black
+        setUpHierachy()
+        setUpLayout()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setUpHierachy() {
+        [titleLabel, addButton, planTableView].forEach {
+            view.addSubview($0)
+        }
     }
-    */
+    
+    private func setUpLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalToSuperview()
+                .inset(LayoutConstants.spacing)
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.trailing.equalToSuperview()
+                .inset(LayoutConstants.spacing)
+            $0.size.equalTo(LayoutConstants.buttonSize)
+        }
+        
+        planTableView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+                .offset(LayoutConstants.planTableViewTopOffset)
+            $0.leading.trailing.equalToSuperview()
+                .inset(LayoutConstants.spacing)
+            $0.bottom.equalToSuperview().inset(LayoutConstants.spacing)
+        }
+    }
+    
+    private func configure() {
+        planTableView.delegate = self
+        planTableView.dataSource = self
+    }
+}
 
+// MARK: - TableView
+extension PlanView: UITableViewDelegate, UITableViewDataSource {
+    
+}
+
+private enum TextConstants {
+    static let title = "Plan"
+    static let plus = "plus"
+}
+
+private enum LayoutConstants {
+    static let spacing: CGFloat = 8
+    static let buttonSize = CGSize(width: 44.44, height: 44.44)
+    static let planTableViewTopOffset: CGFloat = 20
+    static let borderWidth: CGFloat = 1
+    static let cornerRadius: CGFloat = 10
+    static let cellHeight: CGFloat = 100
 }
---
+++ YeolmokTravel/YeolmokTravel/Utilities/AppNamespace.swift
@@ -0,0 +1,14 @@
+//
+//  AppNamespace.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2022/12/20.
+//
+
+import UIKit
+import Foundation
+
+enum AppStyles {
+    static let titleFontSize: CGFloat = 30
+    static let mainColor = UIColor.systemGreen
+}
