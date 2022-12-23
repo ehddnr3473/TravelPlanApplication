b3f83cbc5bdcb286992ede3a41bee0795f7d2367
Donguk Kim <ehddnr73@naver.com>
2022-12-23 20:29:22 +0900

    MemoryView 구성

--- YeolmokTravel/YeolmokTravel/Memory/MemoryView.swift
+++ YeolmokTravel/YeolmokTravel/Memory/MemoryView.swift
@@ -7,9 +7,106 @@
 
 import UIKit
 
-class MemoryView: UIViewController {
-
+final class MemoryView: UIViewController {
+    // MARK: - Properties
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = TextConstants.title
+        label.font = .boldSystemFont(ofSize: AppStyles.titleFontSize)
+        
+        return label
+    }()
+    
+    private lazy var addButton: UIButton = {
+        let button = UIButton(type: .custom)
+        
+        button.setBackgroundImage(UIImage(systemName: TextConstants.plusIconName), for: .normal)
+        button.tintColor = AppStyles.mainColor
+        button.addTarget(self, action: #selector(touchUpAddButton), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    private lazy var memoriesCollectionView: UICollectionView = {
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: createCompositionalLayout())
+        
+        return collectionView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpUI()
     }
+}
+
+// MARK: - SetUp View
+extension MemoryView {
+    private func setUpUI() {
+        view.backgroundColor = .black
+        setUpHierachy()
+        setUpLayout()
+    }
+    
+    private func setUpHierachy() {
+        [titleLabel, addButton, memoriesCollectionView].forEach {
+            view.addSubview($0)
+        }
+    }
+    
+    private func setUpLayout() {
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            $0.leading.equalToSuperview()
+                .inset(LayoutConstants.spacing)
+        }
+        
+        addButton.snp.makeConstraints {
+            $0.centerY.equalTo(titleLabel.snp.centerY)
+            $0.trailing.equalToSuperview()
+                .inset(LayoutConstants.spacing)
+            $0.size.equalTo(LayoutConstants.buttonSize)
+        }
+        
+        memoriesCollectionView.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom)
+                .offset(LayoutConstants.memoriesCollectionViewTopOffset)
+            $0.leading.equalToSuperview().inset(LayoutConstants.spacing)
+            $0.trailing.equalToSuperview().inset(LayoutConstants.spacing)
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+                .inset(LayoutConstants.spacing)
+        }
+    }
+    
+    private func createCompositionalLayout() -> UICollectionViewCompositionalLayout {
+        let item = NSCollectionLayoutItem(layoutSize:
+                                            NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                   heightDimension: .fractionalHeight(1.0)))
+        let group = NSCollectionLayoutGroup.vertical(layoutSize:
+                                                        NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                                               heightDimension: .fractionalHeight(0.7)), subitems: [item])
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
+    
+    
+    @objc func touchUpAddButton() {
+        
+    }
+}
+
+private enum TextConstants {
+    static let title = "Memories"
+    static let plusIconName = "plus"
+}
+
+private enum LayoutConstants {
+    static let spacing: CGFloat = 8
+    static let buttonSize = CGSize(width: 44.44, height: 44.44)
+    static let memoriesCollectionViewTopOffset: CGFloat = 20
 }
