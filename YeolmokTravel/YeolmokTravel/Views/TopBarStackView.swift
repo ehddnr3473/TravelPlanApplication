7161e567e292441536ee8d62ccb51fe03f7afbfb
Donguk Kim <ehddnr73@naver.com>
2022-12-24 15:49:09 +0900

    중복되는 TopBarStackView 코드 문제를 해결하기 위해 커스텀 UIView 클래스 생성

---
+++ YeolmokTravel/YeolmokTravel/Views/TopBarStackView.swift
@@ -0,0 +1,22 @@
+//
+//  TopBarStackView.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2022/12/24.
+//
+
+import UIKit
+
+/// Modal View 상단 바
+class TopBarStackView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    
+}
