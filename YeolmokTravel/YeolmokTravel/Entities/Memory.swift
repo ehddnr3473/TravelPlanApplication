112ac45a613c1d5eaca8227687de04f85043e718
Donguk Kim <ehddnr73@naver.com>
2022-12-24 15:16:58 +0900

    엔티티 폴더 하위로 이동

---
+++ YeolmokTravel/YeolmokTravel/Entities/Memory.swift
@@ -0,0 +1,15 @@
+//
+//  Memory.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2022/12/24.
+//
+
+import Foundation
+
+/// Memory Entity
+struct Memory {
+    var title: String
+    var imageName: String
+    var uploadDate: Date
+}
