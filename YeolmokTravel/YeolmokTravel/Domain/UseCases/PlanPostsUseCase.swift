eb69d07417ce22ae7e33fb3a9b57c814eb9ebad4
Donguk Kim <ehddnr73@naver.com>
2023-01-22 17:05:26 +0900

    PlanPostsUseCase 생성

---
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/PlanPostsUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  PlanPostsUseCase.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/22.
+//
+
+import Foundation
+
+protocol PlanPostsUseCase {
+    func save()
+    func update()
+    func delete()
+}
