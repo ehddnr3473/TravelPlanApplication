31ad3cf107ad2b9ee12c07ddd3a0d1a43f7727e5
Donguk Kim <ehddnr73@naver.com>
2023-01-23 02:26:43 +0900

    Memory upload 함수 추가

--- YeolmokTravel/YeolmokTravel/Domain/UseCases/MemoryPostsUseCase.swift
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/MemoryPostsUseCase.swift
@@ -1,5 +1,5 @@
 //
-//  MemoryDeleteUseCase.swift
+//  MemoryPostsUseCase.swift
 //  YeolmokTravel
 //
 //  Created by 김동욱 on 2023/01/23.
@@ -7,8 +7,12 @@
 
 import Foundation
 
-struct MemoryDeleteUseCase {
+struct MemoryPostsUseCase {
     private let repository = MemoryRepository()
+    
+    func upload(_ memory: Memory) {
+        Task { await repository.writeMemory(memory) }
+    }
     
     func delete(_ index: Int) {
         Task { await repository.delete(at: index) }
