89676094c030201d70064720910f5e7471867d79
Donguk Kim <ehddnr73@naver.com>
2023-01-24 10:41:24 +0900

    Memory 전송을 위한 MemoryTransfer 프로토콜 생성

---
+++ YeolmokTravel/YeolmokTravel/Protocols/MemoryTransfer.swift
@@ -0,0 +1,12 @@
+//
+//  MemoryTransfer.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/24.
+//
+
+import Foundation
+
+protocol MemoryTransfer {
+    func writingHandler(_ memory: Memory)
+}
