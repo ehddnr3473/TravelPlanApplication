22eb44a16a3bb7946501b59f66986c025fcc3e11
Donguk Kim <ehddnr73@naver.com>
2023-01-24 17:11:35 +0900

    접근제어자 수정

--- YeolmokTravel/YeolmokTravel/Domain/Models/Memory.swift
+++ YeolmokTravel/YeolmokTravel/Domain/Models/Memory.swift
@@ -5,12 +5,11 @@
 //  Created by 김동욱 on 2022/12/24.
 //
 
-import UIKit
 import Foundation
 
 /// Memory Model
 struct Memory {
-    var title: String
-    var index: Int
-    var uploadDate: Date
+    let title: String
+    let index: Int
+    let uploadDate: Date
 }
