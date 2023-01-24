810d64bdb1939ae2e716820de37fe13bfa418cb9
Donguk Kim <ehddnr73@naver.com>
2023-01-24 19:11:58 +0900

    view controller builder 생성

---
+++ YeolmokTravel/YeolmokTravel/Builder/MemoryViewBuilder.swift
@@ -0,0 +1,38 @@
+//
+//  MemoryViewBuilder.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/24.
+//
+
+import Foundation
+import UIKit
+
+struct MemoryViewBuilder {
+    private var memoryView: MemoryView
+    private var memoryRepository: MemoryRepository
+    
+    init(memoryView: MemoryView, memoryRepository: MemoryRepository) {
+        self.memoryView = memoryView
+        self.memoryRepository = memoryRepository
+    }
+    
+    private func downloadModel() async -> [Memory] {
+        await memoryRepository.downloadMemories().map { $0.toDomain() }
+    }
+    
+    private func setUpUseCase(_ model: [Memory]) -> DefaultMemoryUseCase {
+        DefaultMemoryUseCase(memories: model)
+    }
+    
+    private func setUpViewModel(_ useCase: DefaultMemoryUseCase) {
+        memoryView.viewModel = MemoryViewModel(useCase: useCase)
+    }
+    
+    func build() async -> MemoryView {
+        let model = await downloadModel()
+        let useCase = setUpUseCase(model)
+        setUpViewModel(useCase)
+        return memoryView
+    }
+}
---
+++ YeolmokTravel/YeolmokTravel/Builder/PlanViewBuilder.swift
@@ -0,0 +1,37 @@
+//
+//  PlanViewBuilder.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/24.
+//
+
+import Foundation
+
+struct PlanViewBuilder {
+    private var planView: PlanView
+    private var planRepository: PlanRepository
+    
+    init(planView: PlanView, planRepository: PlanRepository) {
+        self.planView = planView
+        self.planRepository = planRepository
+    }
+    
+    private func downloadModel() async -> OwnTravelPlan {
+        OwnTravelPlan(travelPlans: await planRepository.read().map { $0.toDomain() })
+    }
+    
+    private func setUpUseCase(_ model: OwnTravelPlan) -> DefaultPlanUseCase {
+        DefaultPlanUseCase(model: model)
+    }
+    
+    private func setUpViewModel(_ useCase: DefaultPlanUseCase) {
+        planView.viewModel = TravelPlaner(useCase)
+    }
+    
+    func build() async -> PlanView {
+        let model = await downloadModel()
+        let useCase = setUpUseCase(model)
+        setUpViewModel(useCase)
+        return planView
+    }
+}
