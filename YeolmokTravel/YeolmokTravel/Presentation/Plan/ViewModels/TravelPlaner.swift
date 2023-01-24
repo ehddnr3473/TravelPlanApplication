8d9c3ece985b40e5d5657f9d06316c79da300d79
Donguk Kim <ehddnr73@naver.com>
2023-01-24 11:46:57 +0900

    use case 의존성 주입

--- YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/TravelPlaner.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/TravelPlaner.swift
@@ -20,72 +20,40 @@ protocol PlanConfigurable: AnyObject {
     func title(_ index: Int) -> String
     func date(_ index: Int) -> String
     func description(_ index: Int) -> String
-    func setUpWritingView(at index: Int?, _ writingStyle: WritingStyle) -> UINavigationController
     
-    init(_ model: OwnTravelPlan)
+    init(_ useCase: DefaultPlanUseCase)
 }
 
 /// TravelPlan View Model
-final class TravelPlaner: ObservableObject, PlanConfigurable, PlanTransfer {
-    var model: OwnTravelPlan
-    
+final class TravelPlaner: PlanConfigurable {
+    private let useCase: DefaultPlanUseCase
     var publisher = PassthroughSubject<Void, Never>()
     
     var planCount: Int {
-        model.travelPlans.count
+        useCase.planCount
     }
     
-    required init(_ model: OwnTravelPlan) {
-        self.model = model
+    required init(_ useCase: DefaultPlanUseCase) {
+        self.useCase = useCase
     }
     
     func title(_ index: Int) -> String {
-        model.travelPlans[index].title
+        useCase.title(index)
     }
     
     func date(_ index: Int) -> String {
-        model.travelPlans[index].date
+        useCase.date(index)
     }
     
     func description(_ index: Int) -> String {
-        model.travelPlans[index].description 
+        useCase.date(index)
     }
     
     func delete(_ index: Int) {
-        Task { await model.delete(at: index) }
+        Task { await useCase.delete(index) }
     }
     
-    // input
-    func writingHandler(_ plan: some Plan, _ index: Int?) {
-        guard let plan = plan as? TravelPlan else { return }
-        if let index = index {
-            model.update(at: index, plan)
-            Task { await model.write(at: index) }
-            publisher.send()
-        } else {
-            model.add(plan)
-            Task { await model.write(at: nil) }
-            publisher.send()
-        }
-    }
-    
-    // 여행 계획을 작성(수정, 추가)하기 위해 프레젠테이션할 ViewController 반환
     func setUpWritingView(at index: Int? = nil, _ writingStyle: WritingStyle) -> UINavigationController {
-        let writingView = WritingTravelPlanViewController()
-        switch writingStyle {
-        case .add:
-            let model = TravelPlan(title: "", description: "", schedules: [])
-            writingView.model = model
-            writingView.addDelegate = self
-        case .edit:
-            let model = model.travelPlans[index!]
-            writingView.model = model
-            writingView.editDelegate = self
-            writingView.planListIndex = index
-        }
-        writingView.writingStyle = writingStyle
-        let navigationController = UINavigationController(rootViewController: writingView)
-        navigationController.modalPresentationStyle = .fullScreen
-        return navigationController
+        useCase.setUpWritingView(at: index, writingStyle)
     }
 }
