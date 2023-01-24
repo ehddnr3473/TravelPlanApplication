104ade7e24522152403fb925bf875589f31d4be5
Donguk Kim <ehddnr73@naver.com>
2023-01-24 09:10:11 +0900

    Memory를 다운로드하는 use case 생성

---
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/MemoryDownloadUseCase.swift
@@ -0,0 +1,30 @@
+//
+//  MemoryDownloadUseCase.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/24.
+//
+
+import Foundation
+
+protocol MemoryDownloadUseCaseType {
+    func download() async
+}
+
+final class MemoryDownloadUseCase {
+    private let repository = MemoryRepository()
+    private(set) var model = [Memory]()
+    
+    var count: Int {
+        model.count
+    }
+    
+    func memories(_ index: Int) -> Memory {
+        model[index]
+    }
+    
+    func download() async {
+        let memories = await repository.readMemories()
+        model = memories
+    }
+}
