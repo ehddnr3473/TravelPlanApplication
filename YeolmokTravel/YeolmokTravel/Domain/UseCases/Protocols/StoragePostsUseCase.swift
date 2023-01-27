7bb29d901060e2300c66b9058d7b0b81692835d8
Donguk Kim <ehddnr73@naver.com>
2023-01-27 15:12:53 +0900

    StoragePostsUseCase 프로토콜 생성

---
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/Protocols/StoragePostsUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  StoragePostsUseCase.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/27.
+//
+
+import Foundation
+import UIKit
+
+protocol StoragePostsUseCase {
+    func upload(_ index: Int, _ image: UIImage) async throws
+    func download(_ index: Int, completion: @escaping ((Result<UIImage, ImageLoadError>) -> Void))
+    func delete(_ index: Int)
+}
