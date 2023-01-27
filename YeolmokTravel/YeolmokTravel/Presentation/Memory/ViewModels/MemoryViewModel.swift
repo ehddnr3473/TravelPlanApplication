66ee8df9f7ba463efc741c61efc0937a5cacf79b
Donguk Kim <ehddnr73@naver.com>
2023-01-27 20:10:14 +0900

    유스케이스 추상화

--- YeolmokTravel/YeolmokTravel/Presentation/Memory/ViewModels/MemoryViewModel.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Memory/ViewModels/MemoryViewModel.swift
@@ -9,19 +9,19 @@ import Foundation
 import Combine
 
 final class MemoryViewModel {
-    private let useCase: DefaultMemoryUseCase
+    private let useCase: ModelControllableUseCase
     private(set) var publisher = PassthroughSubject<Void, Never>()
     
     var count: Int {
         useCase.count
     }
     
-    init(_ useCase: DefaultMemoryUseCase) {
+    init(_ useCase: ModelControllableUseCase) {
         self.useCase = useCase
     }
     
     func memory(_ index: Int) -> Memory {
-        useCase.memory(index)
+        useCase.query(index) as! Memory
     }
     
     func add(_ memory: Memory) {
