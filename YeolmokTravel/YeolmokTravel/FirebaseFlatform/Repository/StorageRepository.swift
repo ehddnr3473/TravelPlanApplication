271679dd07f8fa484e3b8266eb89d4b036069628
Donguk Kim <ehddnr73@naver.com>
2023-01-27 15:25:30 +0900

    StorageRepository 프로토콜 생성

---
+++ YeolmokTravel/YeolmokTravel/FirebaseFlatform/Repository/StorageRepository.swift
@@ -0,0 +1,17 @@
+//
+//  StorageRepository.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/27.
+//
+
+import Foundation
+import UIKit
+
+protocol StorageRepository {
+    func cachedImage(_ index: Int) -> UIImage?
+    func cacheImage(_ index: Int, image: UIImage)
+    func upload(at index: Int, _ image: UIImage) async
+    func download(_ index: Int, _ completion: @escaping ((Result<UIImage, ImageLoadError>) -> Void))
+    func delete(_ index: Int)
+}
