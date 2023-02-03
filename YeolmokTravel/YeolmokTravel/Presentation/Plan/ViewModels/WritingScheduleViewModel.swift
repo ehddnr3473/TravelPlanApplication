5b50f8612ff08fb94a37d81e4b6db6db8f10e8bd
Donguk Kim <ehddnr73@naver.com>
2023-02-03 21:32:31 +0900

    좌표 관련 바인딩 코드 추가

--- YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/WritingScheduleViewModel.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/WritingScheduleViewModel.swift
@@ -8,15 +8,20 @@
 import Foundation
 import Combine
 import UIKit
+import CoreLocation
 
-private protocol WritingScheduleViewModelType {
+private protocol WritingScheduleViewModelType: AnyObject {
     associatedtype TitleInput
     associatedtype TitleOutput
     associatedtype SwitchInput
     associatedtype SwitchOutput
+    associatedtype CoordinateInput
+    associatedtype CoordinateOutput
 }
 
-final class WritingScheduleViewModel {
+final class WritingScheduleViewModel: WritingScheduleViewModelType {
+    private(set) var coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees(), longitude: CLLocationDegrees())
+    
     deinit {
         print("deinit: WritingPlanViewModel")
     }
@@ -40,11 +45,20 @@ final class WritingScheduleViewModel {
         let backgroundColorPublisher: AnyPublisher<UIColor, Never>
     }
     
+    // Coordinate
+    struct CoordinateInput {
+        let latitude: AnyPublisher<String, Never>
+        let longitude: AnyPublisher<String, Never>
+    }
+    
+    struct CoordinateOutput {
+        let buttonState: AnyPublisher<Bool, Never>
+    }
     
     /// UITextField <-> Save UIButton
     /// - Parameter input: Title Text Publisher
     /// - Returns: UIButton - isEnabled Publisher
-    func transform(input: TitleInput) -> TitleOutput {
+    func transform(_ input: TitleInput) -> TitleOutput {
         let buttonStatePublisher = input.title
             .map { $0.count > 0 }
             .eraseToAnyPublisher()
@@ -70,5 +84,21 @@ final class WritingScheduleViewModel {
             .eraseToAnyPublisher()
         
         return SwitchOutput(datePickerStatePublisher: statePublisher, backgroundColorPublisher: backgroundColorPublisher)
+    }
+    
+    /// Coordinate TextFIelds <-> Show Map UIButton
+    /// - Parameter input: Coordinate Text Publisher
+    /// - Returns: UIButton - isEnabled Publisher
+    func transform(_ input: CoordinateInput) -> CoordinateOutput {
+        let buttonStatePublisher = input.latitude.combineLatest(input.longitude)
+            .map { combinedValue in
+                guard let latitude = Double(combinedValue.0), let longitude = Double(combinedValue.1) else { return false }
+                self.coordinate.latitude = latitude
+                self.coordinate.longitude = longitude
+                return CLLocationCoordinate2DIsValid(CLLocationCoordinate2D(latitude: latitude, longitude: longitude))
+            }
+            .eraseToAnyPublisher()
+        
+        return CoordinateOutput(buttonState: buttonStatePublisher)
     }
 }
