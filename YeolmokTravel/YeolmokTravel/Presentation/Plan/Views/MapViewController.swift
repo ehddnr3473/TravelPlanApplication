04f7a4bf662c57cc36d8258a8e4a6e74e2bcdfb5
Donguk Kim <ehddnr73@naver.com>
2023-02-03 19:07:15 +0900

    MKMapView를 보여주는 view controller 추가

---
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/Views/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/02/03.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+final class MapViewController: UIViewController {
+
+    private let mapView: MKMapView = {
+        let mapView = MKMapView()
+        mapView.preferredConfiguration = MKStandardMapConfiguration(emphasisStyle: .muted)
+        return mapView
+    }()
+    
+    var coordinate: CLLocationCoordinate2D!
+    let span: CLLocationDegrees = 0.005
+    var pinTitle: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.addSubview(mapView)
+        configureMapView()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapView.frame = view.frame
+    }
+    
+    private func configureMapView() {
+        mapView.region = MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(latitudeDelta: span, longitudeDelta: span)
+        )
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        annotation.title = pinTitle
+        
+        mapView.addAnnotation(annotation)
+    }
+}
