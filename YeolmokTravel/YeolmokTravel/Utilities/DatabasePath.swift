6312b3472c230db8caca520c46c018f405295c9e
Donguk Kim <ehddnr73@naver.com>
2023-02-04 15:17:25 +0900

    DatabasePath 정의

---
+++ YeolmokTravel/YeolmokTravel/Utilities/DatabasePath.swift
@@ -0,0 +1,13 @@
+//
+//  DatabasePath.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/02/04.
+//
+
+import Foundation
+
+enum DatabasePath {
+    static let plans = "plans"
+    static let memories = "memories"
+}
