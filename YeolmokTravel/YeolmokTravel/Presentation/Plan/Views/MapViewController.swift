225e0789c08ada14c591fd602da0138d9950d323
Donguk Kim <ehddnr73@naver.com>
2023-02-06 03:11:14 +0900

    AppTextConstants.mapViewAccessibilityLabel 적용

--- YeolmokTravel/YeolmokTravel/Presentation/Plan/Views/MapViewController.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/Views/MapViewController.swift
@@ -22,7 +22,7 @@ final class MapViewController: UIViewController {
         mapView.layer.cornerRadius = LayoutConstants.cornerRadius
         mapView.layer.borderWidth = AppLayoutConstants.borderWidth
         mapView.layer.borderColor = UIColor.white.cgColor
-        mapView.accessibilityLabel = "mapView"
+        mapView.accessibilityLabel = AppTextConstants.mapViewAccessibilityLabel
         return mapView
     }()
     
