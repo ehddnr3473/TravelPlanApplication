a3fae3e03000f97bf4ff107f212102fe7ac696db
Donguk Kim <ehddnr73@naver.com>
2023-02-11 19:10:18 +0900

    모델 바인딩 및 리팩터링

--- YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/WritingTravelPlanViewModel.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/ViewModels/WritingTravelPlanViewModel.swift
@@ -9,46 +9,29 @@ import Foundation
 import Combine
 import CoreLocation
 
-private protocol WritingTravelPlanViewModelType: AnyObject {
+private protocol WritingTravelPlanViewModel: AnyObject {
     associatedtype TextInput
     associatedtype TextOutput
     
     func transform(input: TextInput) -> TextOutput
 }
 
-final class WritingTravelPlanViewModel {
-    private(set) var planTracker: PlanTracker<TravelPlan>
-    private(set) var model: TravelPlan
+final class ConcreteWritingTravelPlanViewModel {
+    private(set) var travelPlanTracker: TravelPlanTracker
+    private(set) var model: CurrentValueSubject<TravelPlan, Never>
     
     private var title: String
     private var description: String
     private(set) var coordinatesPublisher = PassthroughSubject<[CLLocationCoordinate2D], Never>()
     private var subscriptions = Set<AnyCancellable>()
     
-    var modelTitle: String {
-        model.title
-    }
-    
-    var modelDescription: String {
-        model.description
-    }
-    
-    var schedules: [Schedule] {
-        model.schedules
-    }
-    
-    var schedulesCount: Int {
-        model.schedules.count
-    }
-    
     var scrollViewContainerheight: CGFloat {
-        if schedulesCount == 0 {
+        if model.value.schedules.count == 0 {
             return AppLayoutConstants.writingTravelPlanViewHeight +
-            Double(schedulesCount) * AppLayoutConstants.cellHeight +
             AppLayoutConstants.largeSpacing * 2
         } else {
             return AppLayoutConstants.writingTravelPlanViewHeight +
-            Double(schedulesCount) * AppLayoutConstants.cellHeight +
+            Double(model.value.schedules.count) * AppLayoutConstants.cellHeight +
             AppLayoutConstants.mapViewHeight +
             AppLayoutConstants.largeFontSize +
             AppLayoutConstants.spacing +
@@ -57,11 +40,11 @@ final class WritingTravelPlanViewModel {
         }
     }
     
-    init(_ model: TravelPlan) {
+    init(_ model: CurrentValueSubject<TravelPlan, Never>) {
         self.model = model
-        self.planTracker = PlanTracker(model)
-        self.title = model.title
-        self.description = model.description
+        self.travelPlanTracker = TravelPlanTracker(model.value)
+        self.title = model.value.title
+        self.description = model.value.description
     }
     
     deinit {
@@ -69,51 +52,41 @@ final class WritingTravelPlanViewModel {
     }
     
     func setTravelPlan() {
-        model.setTravelPlanText(title, description)
+        model.value.setTravelPlanText(title, description)
     }
     
-    func editSchedule(at index: Int, _ schedule: Schedule) {
-        model.editSchedule(at: index, schedule)
-        scheduleControlCompletion()
+    func updateSchedule(at index: Int, _ schedule: Schedule) {
+        model.value.updateSchedule(at: index, schedule)
     }
     
-    func addSchedule(_ schedule: Schedule) {
-        model.addSchedule(schedule)
-        scheduleControlCompletion()
+    func createSchedule(_ schedule: Schedule) {
+        model.value.createSchedule(schedule)
     }
     
     func removeSchedule(at index: Int) {
-        model.removeSchedule(at: index)
-        scheduleControlCompletion()
+        model.value.deleteSchedule(at: index)
     }
     
     func swapSchedules(at source: Int, to destination: Int) {
-        model.swapSchedules(at: source, to: destination)
-        scheduleControlCompletion()
-    }
-    
-    func scheduleControlCompletion() {
-        coordinatesPublisher.send(coordinatesOfSchedules())
-    }
-    
-    func coordinatesOfSchedules() -> [CLLocationCoordinate2D] {
-        var coordinates = [CLLocationCoordinate2D]()
-        
-        for schedule in schedules {
-            coordinates.append(schedule.coordinate)
-        }
-        
-        return coordinates
+        model.value.swapSchedules(at: source, to: destination)
     }
     
     func setPlan() {
-        planTracker.setPlan(TravelPlan(title: title,
-                                       description: description,
-                                       schedules: schedules))
+        travelPlanTracker.setPlan(TravelPlan(title: title,
+                                             description: description,
+                                             schedules: model.value.schedules))
+    }
+    
+    func create(_ schedule: Schedule) {
+        createSchedule(schedule)
+    }
+    
+    func update(at index: Int, _ schedule: Schedule) {
+        updateSchedule(at: index, schedule)
     }
 }
 
-extension WritingTravelPlanViewModel: WritingTravelPlanViewModelType {
+extension ConcreteWritingTravelPlanViewModel: WritingTravelPlanViewModel {
     struct TextInput {
         let titlePublisher: AnyPublisher<String, Never>
         let descriptionPublisher: CurrentValueSubject<String, Never>
