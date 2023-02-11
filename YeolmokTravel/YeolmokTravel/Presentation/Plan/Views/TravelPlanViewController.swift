ca1ddaea5b0498d415c3d8eb12f7907ea845e8b9
Donguk Kim <ehddnr73@naver.com>
2023-02-12 04:09:49 +0900

    바인딩 수정 및 리팩터링

--- YeolmokTravel/YeolmokTravel/Presentation/Plan/Views/TravelPlanViewController.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Plan/Views/TravelPlanViewController.swift
@@ -20,15 +20,6 @@ final class TravelPlanViewController: UIViewController {
     private let viewModel: ConcreteTravelPlanViewModel
     private var subscriptions = Set<AnyCancellable>()
     
-    init(_ viewModel: ConcreteTravelPlanViewModel) {
-        self.viewModel = viewModel
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     private var titleLabel: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -66,6 +57,15 @@ final class TravelPlanViewController: UIViewController {
         return tableView
     }()
     
+    init(_ viewModel: ConcreteTravelPlanViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -73,8 +73,9 @@ final class TravelPlanViewController: UIViewController {
             do {
                 try await viewModel.read()
             } catch {
-                guard let error = error as? TravelPlanRepositoryError else { return }
-                alertWillAppear(error.rawValue)
+                if let error = error as? TravelPlanRepositoryError {
+                    alertWillAppear(error.rawValue)
+                }
             }
         }
         configureView()
@@ -123,7 +124,7 @@ private extension TravelPlanViewController {
                 .offset(LayoutConstants.planTableViewTopOffset)
             $0.leading.trailing.equalToSuperview()
                 .inset(AppLayoutConstants.spacing)
-            $0.height.equalTo(viewModel.model.value.travelPlans.count * Int(LayoutConstants.cellHeight))
+            $0.height.equalTo(viewModel.model.value.count * Int(LayoutConstants.cellHeight))
         }
     }
     
@@ -142,7 +143,7 @@ private extension TravelPlanViewController {
     
     @MainActor func updateTableViewConstraints() {
         planTableView.snp.updateConstraints {
-            $0.height.equalTo(viewModel.model.value.travelPlans.count * Int(LayoutConstants.cellHeight))
+            $0.height.equalTo(viewModel.model.value.count * Int(LayoutConstants.cellHeight))
         }
     }
     
@@ -156,7 +157,7 @@ private extension TravelPlanViewController {
     
     func setBindings() {
         viewModel.model
-            .receive(on: RunLoop.main)
+            .receive(on: DispatchQueue.main)
             .sink { [weak self] _ in self?.reload() }
             .store(in: &subscriptions)
     }
@@ -165,13 +166,31 @@ private extension TravelPlanViewController {
         let model = TravelPlan(title: "", description: "", schedules: [])
         let coordinates = model.coordinates
         let mapViewController = MapViewController(coordinates)
-        let writingView = WritingTravelPlanViewController(ConcreteWritingTravelPlanViewModel(CurrentValueSubject<TravelPlan, Never>(model)), mapViewController, .add)
+        let writingView = WritingTravelPlanViewController(
+            ConcreteWritingTravelPlanViewModel(CurrentValueSubject<TravelPlan, Never>(model)),
+            mapViewController,
+            .create,
+            delegate: self
+        )
         writingView.delegate = self
-//        let navigationController = UINavigationController(rootViewController: writingView)
-        let navigationController = UINavigationController()
-        navigationController.setViewControllers([writingView], animated: false)
-//        navigationController.modalPresentationStyle = .fullScreen
-        present(navigationController, animated: true)
+        writingView.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(writingView, animated: true)
+    }
+    
+    func didSelectRow(_ index: Int) {
+        let model = viewModel.model.value[index]
+        let coordinates = model.coordinates
+        let mapViewController = MapViewController(coordinates)
+        let writingView = WritingTravelPlanViewController(
+            ConcreteWritingTravelPlanViewModel(CurrentValueSubject<TravelPlan, Never>(model)),
+            mapViewController,
+            .update,
+            delegate: self
+        )
+        writingView.delegate = self
+        writingView.planListIndex = index
+        writingView.hidesBottomBarWhenPushed = true
+        navigationController?.pushViewController(writingView, animated: true)
     }
 }
 
@@ -180,14 +199,14 @@ extension TravelPlanViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TravelPlanTableViewCell.identifier, for: indexPath) as? TravelPlanTableViewCell else { return UITableViewCell() }
         
-        cell.titleLabel.text = viewModel.model.value.travelPlans[indexPath.row].title
-        cell.dateLabel.text = viewModel.model.value.travelPlans[indexPath.row].date
-        cell.descriptionLabel.text = viewModel.model.value.travelPlans[indexPath.row].description
+        cell.titleLabel.text = viewModel.model.value[indexPath.row].title
+        cell.dateLabel.text = viewModel.model.value[indexPath.row].date
+        cell.descriptionLabel.text = viewModel.model.value[indexPath.row].description
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        viewModel.model.value.travelPlans.count
+        viewModel.model.value.count
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
@@ -225,15 +244,7 @@ extension TravelPlanViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let model = viewModel.model.value.travelPlans[indexPath.row]
-        let coordinates = model.coordinates
-        let mapViewController = MapViewController(coordinates)
-        let writingView = WritingTravelPlanViewController(ConcreteWritingTravelPlanViewModel(CurrentValueSubject<TravelPlan, Never>(model)), mapViewController, .edit)
-        writingView.delegate = self
-        writingView.planListIndex = indexPath.row
-        let navigationController = UINavigationController(rootViewController: writingView)
-        navigationController.modalPresentationStyle = .fullScreen
-        present(navigationController, animated: true)
+        didSelectRow(indexPath.row)
     }
 }
 
