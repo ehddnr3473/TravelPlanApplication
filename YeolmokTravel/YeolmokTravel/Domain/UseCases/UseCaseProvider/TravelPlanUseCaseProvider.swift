6dc0f37a1ea76543b50e814935e3007a4089e9e6
Donguk Kim <ehddnr73@naver.com>
2023-02-11 19:09:01 +0900

    추상화된 레포지토리를 가지고 유스케이스의 구체 타입을 제공하는 TravelPlanUseCaseProvider 정의

---
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/UseCaseProvider/TravelPlanUseCaseProvider.swift
@@ -0,0 +1,34 @@
+//
+//  UseCaseProvider.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/01/27.
+//
+
+import Foundation
+
+protocol TravelPlanUseCaseProvider {
+    func provideTravelPlanUploadUseCase() -> TravelPlanUploadUseCase
+    func provideTravelPlanReadUseCase() -> TravelPlanReadUseCase
+    func provideTravelPlanDeleteUseCase() -> TravelPlanDeleteUseCase
+}
+
+final class ConcreteTravelPlanUseCaseProvider: TravelPlanUseCaseProvider {
+    private let repository: AbstractTravelPlanRepository
+
+    init(repository: AbstractTravelPlanRepository) {
+        self.repository = repository
+    }
+    
+    func provideTravelPlanUploadUseCase() -> TravelPlanUploadUseCase {
+        ConcreteTravelPlanUploadUseCase(repository)
+    }
+    
+    func provideTravelPlanReadUseCase() -> TravelPlanReadUseCase {
+        ConcreteTravelPlanReadUseCase(repository)
+    }
+    
+    func provideTravelPlanDeleteUseCase() -> TravelPlanDeleteUseCase {
+        ConcreteTravelPlanDeleteUseCase(repository)
+    }
+}
