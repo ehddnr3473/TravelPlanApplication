3cd5be40120a2c9780e3e6d3fc78801cc653e2aa
Donguk Kim <ehddnr73@naver.com>
2023-02-12 21:57:10 +0900

    TravelPlanSwapUseCase 정의

---
+++ YeolmokTravel/YeolmokTravel/Domain/UseCases/TravelPlanSwapUseCase.swift
@@ -0,0 +1,51 @@
+//
+//  TravelPlanSwapUseCase.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/02/12.
+//
+
+import Foundation
+
+struct TravelPlanSwapBox {
+    let source: Int
+    let destination: Int
+    let sourceTravelPlan: TravelPlan
+    let destinationTravelPlan: TravelPlan
+}
+
+protocol TravelPlanSwapUseCase: AnyObject {
+    /// 데이터베이스에서 순서를 swap하는 유스케이스 메서드
+    /// - sourceTravelPlan과 destinationTravelPlan의 업로드 성공시 모델 업데이트
+    /// - 둘 중 하나라도 실패시, error throw
+    /// - Parameter travelPlanSwapBox: swap을 수행하기 위한 데이터 묶음
+    func execute(_ travelPlanSwapBox: TravelPlanSwapBox) async throws
+}
+
+final class ConcreteTravelPlanSwapUseCase: TravelPlanSwapUseCase {
+    private let repository: AbstractTravelPlanRepository
+    
+    init(_ repository: AbstractTravelPlanRepository) {
+        self.repository = repository
+    }
+    
+    func execute(_ travelPlanSwapBox: TravelPlanSwapBox) async throws {
+        try await withThrowingTaskGroup(of: Void.self) { taskGroup in // rethrows
+            taskGroup.addTask { [self] in
+                try await repository.upload(
+                    at: travelPlanSwapBox.source,
+                    travelPlanDTO: travelPlanSwapBox.sourceTravelPlan.toData()
+                )
+            }
+            
+            taskGroup.addTask { [self] in
+                try await repository.upload(
+                    at: travelPlanSwapBox.destination,
+                    travelPlanDTO: travelPlanSwapBox.destinationTravelPlan.toData()
+                )
+            }
+            
+            for try await _ in taskGroup { }
+        }
+    }
+}
