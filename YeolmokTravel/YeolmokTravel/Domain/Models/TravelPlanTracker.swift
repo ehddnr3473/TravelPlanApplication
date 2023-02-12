eab78c10e6d4945c284a48d500afabd5cca590c1
Donguk Kim <ehddnr73@naver.com>
2023-02-12 22:32:41 +0900

    리팩터링
    - 기존에 private 접근 제어 수준이던 travelPlan을 직접 접근할 수 있도록 내부 접근 수준(internal)으로 변경.
    - 따라서 불필요해진 메서드 제거

--- YeolmokTravel/YeolmokTravel/Domain/Models/TravelPlanTracker.swift
+++ YeolmokTravel/YeolmokTravel/Domain/Models/TravelPlanTracker.swift
@@ -9,7 +9,7 @@ import Foundation
 
 /// TravelPlan의 변경 사항을 추적하고 alert을 띄워주기 위한 데이터 struct
 struct TravelPlanTracker {
-    private var travelPlan: TravelPlan
+    var travelPlan: TravelPlan
     private let initialTravelPlan: TravelPlan
     
     // 변경된 내용을 확인하기 위해 초기값 저장
@@ -24,10 +24,5 @@ struct TravelPlanTracker {
         } else {
             return true
         }
-    }
-    
-    // update
-    mutating func setPlan(_ travelPlan: TravelPlan) {
-        self.travelPlan = travelPlan
     }
 }
