20cd1a98d800dd3806cc861029d6ea40eed77d8f
Donguk Kim <ehddnr73@naver.com>
2023-02-20 07:03:10 +0900

    Domain layer xcframework 형태로 종속성 추가

---
+++ YeolmokTravel/YeolmokTravel/Frameworks/FirebasePlatform.framework/Headers/FirebasePlatform.h
@@ -0,0 +1,18 @@
+//
+//  FirebasePlatform.h
+//  FirebasePlatform
+//
+//  Created by 김동욱 on 2023/02/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebasePlatform.
+FOUNDATION_EXPORT double FirebasePlatformVersionNumber;
+
+//! Project version string for FirebasePlatform.
+FOUNDATION_EXPORT const unsigned char FirebasePlatformVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebasePlatform/PublicHeader.h>
+
+
