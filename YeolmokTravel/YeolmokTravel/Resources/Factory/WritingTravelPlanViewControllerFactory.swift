b22afd529afd13e70df9aba2ae61e4a566cce008
Donguk Kim <ehddnr73@naver.com>
2023-02-21 22:53:39 +0900

    WritingTravelPlanViewControllerFactory, 인프라 스트럭처 정의

---
+++ YeolmokTravel/YeolmokTravel/Resources/Factory/WritingTravelPlanViewControllerFactory.swift
@@ -0,0 +1,25 @@
+//
+//  WritingTravelPlanViewControllerFactory.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/02/21.
+//
+
+import Foundation
+
+struct WritingTravelPlanViewControllerFactory {
+    func createWritingTravelPlanViewController(with model: YTTravelPlan,
+                                               writingStyle: WritingStyle,
+                                               delegate: TravelPlanTransferDelegate,
+                                               travelPlanListIndex: Int?) -> WritingTravelPlanViewController {
+        let writingTravelPlanView = WritingTravelPlanViewController(
+            viewModel: ConcreteWritingTravelPlanViewModel(model),
+            mapProvider: MapViewController(model.coordinates),
+            writingStyle: writingStyle,
+            delegate: delegate,
+            travelPlanListIndex: travelPlanListIndex
+        )
+        writingTravelPlanView.hidesBottomBarWhenPushed = true
+        return writingTravelPlanView
+    }
+}
