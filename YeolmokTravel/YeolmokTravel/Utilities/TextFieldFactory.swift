2e5ac08d0cced1fa64e9e66c0377e72331e5fa21
Donguk Kim <ehddnr73@naver.com>
2023-02-22 09:56:31 +0900

    중복되는 titleTextFIeld 코드를 처리하기 위해 TextFieldFactory 유틸리티 클래스 정의

---
+++ YeolmokTravel/YeolmokTravel/Utilities/TextFieldFactory.swift
@@ -0,0 +1,35 @@
+//
+//  TextFieldFactory.swift
+//  YeolmokTravel
+//
+//  Created by 김동욱 on 2023/02/22.
+//
+
+import Foundation
+import UIKit
+
+final class TextFieldFactory {
+    enum LayoutConstants {
+        static let cornerRadius: CGFloat = 5
+    }
+    
+    static func makeTitleTextField(_ fontSize: CGFloat, _ placeholder: String) -> UITextField {
+        let textField = UITextField()
+        textField.textColor = .white
+        textField.backgroundColor = .systemBackground
+        textField.layer.cornerRadius = LayoutConstants.cornerRadius
+        textField.layer.borderWidth = AppLayoutConstants.borderWidth
+        textField.layer.borderColor = UIColor.white.cgColor
+        textField.font = .boldSystemFont(ofSize: fontSize)
+        textField.placeholder = placeholder
+        textField.autocorrectionType = .no
+        textField.autocapitalizationType = .none
+        textField.returnKeyType = .done
+        textField.leftView = UIView(frame: CGRect(x: .zero,
+                                                  y: .zero,
+                                                  width: AppLayoutConstants.spacing,
+                                                  height: .zero))
+        textField.leftViewMode = .always
+        return textField
+    }
+}
