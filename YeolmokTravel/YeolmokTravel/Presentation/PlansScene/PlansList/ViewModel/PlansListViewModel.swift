372faf920b3b5260bbb22c90a8e5995247ea0c4e
Donguk Kim <ehddnr73@naver.com>
2023-02-24 23:24:00 +0900

    리팩토링
    - 추상화 도입
    - 클래스 이름 변경
    - 메서드 정리

--- YeolmokTravel/YeolmokTravel/Presentation/PlansScene/PlansList/ViewModel/PlansListViewModel.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/PlansScene/PlansList/ViewModel/PlansListViewModel.swift
@@ -1,5 +1,5 @@
 //
-//  TravelPlanViewModel.swift
+//  PlansListViewModel.swift
 //  YeolmokTravel
 //
 //  Created by 김동욱 on 2022/12/20.
@@ -10,44 +10,54 @@ import Combine
 import Domain
 import FirebasePlatform
 
-private protocol TravelPlanViewModel: AnyObject {
-    // Input(Model update) -> Output(Model information)
-    func create(_ travelPlan: YTTravelPlan) async throws
-    func read() async throws
-    func update(at index: Int, _ travelPlan: YTTravelPlan) async throws
+protocol PlansListViewModelInput {
+    func create(_ plan: Plan) async throws
+    func update(at index: Int, _ plan: Plan) async throws
     func delete(_ index: Int) async throws
     func swapTravelPlans(at source: Int, to destination: Int) async throws
 }
 
-final class ConcreteTravelPlanViewModel: TravelPlanViewModel {
-    private(set) var model = CurrentValueSubject<[YTTravelPlan], Never>([])
+protocol PlansListViewModelOutput {
+    var plans: CurrentValueSubject<[Plan], Never> { get }
+    func read() async throws // viewDidLoad
+}
+
+protocol PlansListViewModel: PlansListViewModelInput, PlansListViewModelOutput, AnyObject {}
+
+final class DefaultPlansListViewModel: PlansListViewModel {
     private let useCaseProvider: TravelPlanUseCaseProvider
+    // MARK: - Output
+    let plans = CurrentValueSubject<[Plan], Never>([])
     
+    // MARK: - Init
     init(_ useCaseProvider: TravelPlanUseCaseProvider) {
         self.useCaseProvider = useCaseProvider
     }
     
-    func create(_ travelPlan: YTTravelPlan) async throws {
-        let uploadUseCase = useCaseProvider.provideTravelPlanUploadUseCase()
-        try await uploadUseCase.execute(at: model.value.endIndex, travelPlan: travelPlan.toDomain())
-        model.value.append(travelPlan)
-    }
-    
     func read() async throws {
         let readUseCase = useCaseProvider.provideTravelPlanReadUseCase()
-        model.send(try await readUseCase.execute().map { YTTravelPlan(travelPlan: $0) })
+        plans.send(try await readUseCase.execute().map { Plan(travelPlan: $0) })
+    }
+}
+
+// MARK: - Input
+extension DefaultPlansListViewModel {
+    func create(_ plan: Plan) async throws {
+        let uploadUseCase = useCaseProvider.provideTravelPlanUploadUseCase()
+        try await uploadUseCase.execute(at: plans.value.endIndex, travelPlan: plan.toDomain())
+        plans.value.append(plan)
     }
     
-    func update(at index: Int, _ travelPlan: YTTravelPlan) async throws {
+    func update(at index: Int, _ plan: Plan) async throws {
         let uploadUseCase = useCaseProvider.provideTravelPlanUploadUseCase()
-        try await uploadUseCase.execute(at: index, travelPlan: travelPlan.toDomain())
-        model.value[index] = travelPlan
+        try await uploadUseCase.execute(at: index, travelPlan: plan.toDomain())
+        plans.value[index] = plan
     }
     
     func delete(_ index: Int) async throws {
         let deleteUseCase = useCaseProvider.provideTravelPlanDeleteUseCase()
         try await deleteUseCase.execute(at: index)
-        model.value.remove(at: index)
+        plans.value.remove(at: index)
     }
     
     func swapTravelPlans(at source: Int, to destination: Int) async throws {
@@ -57,19 +67,19 @@ final class ConcreteTravelPlanViewModel: TravelPlanViewModel {
                 TravelPlanSwapBox(
                     source: source,
                     destination: destination,
-                    sourceTravelPlan: model.value[source].toDomain(),
-                    destinationTravelPlan: model.value[source].toDomain()
+                    sourceTravelPlan: plans.value[source].toDomain(),
+                    destinationTravelPlan: plans.value[source].toDomain()
                 )
             )
-            // swap에 성공했다면, 모델 업데이트
-            model.value.swapAt(source, destination)
+            // swap에 성공했다면, 업데이트
+            plans.value.swapAt(source, destination)
         } catch {
             /*
              swap(총 2번의 upload(at:travelPlanDTO:))을 하며 sourceTravelPlan, destinationTravelPlan 둘 중 하나의,
              또는 둘 다의 업로드에 실패했다면, 초기 상태로 돌리기 위해 각각 update(at:_:) 수행
              */
-            try? await update(at: source, model.value[source])
-            try? await update(at: destination, model.value[destination])
+            try? await update(at: source, plans.value[source])
+            try? await update(at: destination, plans.value[destination])
             throw TravelPlanRepositoryError.swapError
         }
     }
