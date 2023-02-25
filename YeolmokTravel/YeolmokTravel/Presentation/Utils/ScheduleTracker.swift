77b42c5a6e0a98005bb61d33e08a2ad3d92e3673
Donguk Kim <ehddnr73@naver.com>
2023-02-25 17:02:40 +0900

    엔티티 변경사항 적용

--- YeolmokTravel/YeolmokTravel/Presentation/Utils/ScheduleTracker.swift
+++ YeolmokTravel/YeolmokTravel/Presentation/Utils/ScheduleTracker.swift
@@ -1,19 +1,19 @@
 //
-//  YTScheduleTracker.swift
+//  ScheduleTracker.swift
 //  YeolmokTravel
 //
 //  Created by 김동욱 on 2023/02/10.
 //
 
 import Foundation
+import Domain
 
-/// YTSchedule의 변경 사항을 추적하고 alert을 띄워주기 위한 데이터 struct
-struct YTScheduleTracker {
-    var schedule: YTSchedule
-    private let initialSchedule: YTSchedule
+/// Schedule의 변경 사항을 추적하고 alert을 띄워주기 위한 데이터 struct
+struct ScheduleTracker {
+    var schedule: Schedule
+    private let initialSchedule: Schedule
     
-    // 변경된 내용을 확인하기 위해 초기값 저장
-    init(_ schedule: YTSchedule) {
+    init(_ schedule: Schedule) {
         self.schedule = schedule
         self.initialSchedule = schedule
     }
