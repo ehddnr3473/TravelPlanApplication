3e8a64b6e1ae91ce879305d958923ac81dc04dd5
Donguk Kim <ehddnr73@naver.com>
2023-02-27 19:52:54 +0900

    MemoryCellViewModelTests 정의

---
+++ YeolmokTravel/YeolmokTravelTests/MemoryCellViewModelTests.swift
@@ -0,0 +1,47 @@
+//
+//  MemoryCellViewModelTests.swift
+//  YeolmokTravelTests
+//
+//  Created by 김동욱 on 2023/02/27.
+//
+
+import XCTest
+@testable import YeolmokTravel
+import Domain
+import Combine
+
+final class MemoryCellViewModelTests: XCTestCase {
+    
+    func testRead() {
+        // given
+        let repository = ImagesRepositoryMock()
+        let imageExpectation = expectation(description: "Image received")
+        let useCaseProvider = DefaultImagesUseCaseProvider(repository: repository)
+        
+        let memory = YTMemory(title: "title2", index: 2, uploadDate: Date())
+        let expectedImage = UIImage(systemName: "2.circle")!
+        
+        let viewModel = DefaultMemoryCellViewModel(memory, useCaseProvider)
+        var cancellables = Set<AnyCancellable>()
+        
+        viewModel.imagePublisher
+            .sink(receiveCompletion: { completion in
+                switch completion {
+                case .failure(let error):
+                    XCTFail("imagePublisher failed with Error \(error)")
+                case .finished:
+                    break
+                }
+            }) { image in
+                // then
+                XCTAssertEqual(image, expectedImage)
+                imageExpectation.fulfill()
+            }
+            .store(in: &cancellables)
+        
+        // when
+        viewModel.read()
+        
+        waitForExpectations(timeout: 5)
+    }
+}
